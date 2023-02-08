8be5674042617eb7c8d7b0b0742c5ce056d63b6a
waveofmymind <93868431+waveofmymind@users.noreply.github.com>
2023-02-09 02:35:43 +0900

    feat: member 엔티티 및 service

---
+++ src/main/java/wave/jwtpractice/member/dto/MemberRequestDto.java
@@ -0,0 +1,33 @@
+package wave.jwtpractice.member.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import wave.jwtpractice.member.Member;
+import wave.jwtpractice.member.RoleType;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class MemberRequestDto {
+    private String email;
+    private String password;
+    private String nickname;
+
+    public Member toMember(PasswordEncoder passwordEncoder) {
+        return Member.builder()
+                .email(email)
+                .password(passwordEncoder.encode(password))
+                .nickname(nickname)
+                .roleType(RoleType.ROLE_USER)
+                .build();
+    }
+
+    public UsernamePasswordAuthenticationToken toAuthentication() {
+        return new UsernamePasswordAuthenticationToken(email, password);
+    }
+}
